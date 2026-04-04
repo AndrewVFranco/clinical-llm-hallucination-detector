5f25b347d7029c17b95da5a285a5b11ad80ea30f
AndrewVFranco <129307231+AndrewVFranco@users.noreply.github.com>
2026-04-04 02:54:34 -0700

    Fix path to key in config

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,5 @@
 from pydantic_settings import BaseSettings
-
+from pathlib import Path
 
 class Settings(BaseSettings):
     # NCBI
@@ -27,6 +27,6 @@ class Settings(BaseSettings):
     DEBUG: bool = True
 
     class Config:
-        env_file = ".env"
+        env_file = Path(__file__).resolve().parent.parent.parent / ".env"
 
 settings = Settings()
